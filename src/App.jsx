6b858fb65c31c1a1fc4412d4cfabfc1de14c3ec2
Aayush09123 <110332733+Aayush09123@users.noreply.github.com>
2024-03-19 15:32:00 +0530

    render all todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
+import { Store } from './app/Store';
 import AddTodo from './components/AddTodo';
+import Todos from './components/Todos';
+import { Provider } from "react-redux";
 
 function App() {
 
   return (
-    <>
+    <Provider store={Store}>
       <h1 className='text-3xl'>Learn about Redux Toolkit</h1>
       <AddTodo />
-    </>
+      <Todos />
+    </Provider>
   )
 }
 
